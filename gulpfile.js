b79c343aa4028cfba42b67752ff66887a8439c83
Balakrishnan (Balki) Ranganathan <balki@live.com>
2015-08-01 11:30:50 -0700

    Added gulp-if module Added maya folder path on Build task to copy script automatically Added conditional gulpif directory exists for Build task's mayaScriptFolder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-var gulp = require("gulp"),
-	rimraf = require("rimraf"),
-	concat = require("gulp-concat");
+var fs = require('fs'),
+	gulp = require('gulp'),
+	rimraf = require('rimraf'),
+	concat = require('gulp-concat'),
+	gulpif = require('gulp-if');
+
+var mayaScriptsFolder = '/Users/Balki/Documents/maya/2016/scripts/';
+
 
 gulp.task('Clean', function (cb) {
 	rimraf('./Out', cb);
@@ -9,7 +14,16 @@ gulp.task('Clean', function (cb) {
 gulp.task('Build', ['Clean'], function () {
 	gulp.src(['./Scripts/Common.py', './Scripts/Sequencer.py'])
 		.pipe(concat('Sequencer.py'))
-		.pipe(gulp.dest('./Out/'));
+		.pipe(gulp.dest('./Out/'))
+		.pipe(gulpif(function () {
+			try {
+				var stats = fs.lstatSync(mayaScriptsFolder);
+				return stats.isDirectory();
+			}
+			catch (e) {
+				return false;
+			}
+		}, gulp.dest(mayaScriptsFolder)));
 });
 
 gulp.task('default', ['Build']);
